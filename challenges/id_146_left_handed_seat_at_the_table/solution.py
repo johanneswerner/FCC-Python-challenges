9dbf1ef6c4529cc0f7243759e715c6345c40b89e
Johannes Werner <johannes.werner@cogitars.com>
2026-01-03 19:21:05 +0100

    Refactor and enhance "Left-Handed Seat at the Table" solution with type hints, improved docstrings, and parametrized tests

--- challenges/id_146_left_handed_seat_at_the_table/solution.py
+++ challenges/id_146_left_handed_seat_at_the_table/solution.py
@@ -1,14 +1,15 @@
-"""This module deals with the determination of the number of left handed seats at the table."""
+"""This module deals with determining the number of left-handed seats at the table."""
 
 
 def find_left_handed_seats(table: list[list[str]]) -> int:
     """Determine the number of left handed seats at the table.
 
     Args:
-        table (list[list[str]]): The seating arrangement at the table, where each sublist represents a row of seats.
-            Each element in the sublist is either 'U' for an upright position, 'L' for a left-handed position, or 'R'
-            for a right-handed position. The imaginary persons in the upper list are facing down, the imaginary persons
-            in the lower list are facing up.
+        table (list[list[str]]): The seating arrangement at the table, where each
+        sublist represents a row of seats. Each element in the sublist is either 'U'
+        for an upright position, 'L' for a left-handed position, or 'R' for a
+        right-handed position. The imaginary persons in the upper list are facing down,
+        the imaginary persons in the lower list are facing up.
 
     Returns:
         int: The number of left handed seats at the table.

6b2103174d22f6a82a06e653ba8f6d442cf120cb
Johannes Werner <johannes.werner@cogitars.com>
2026-01-06 09:05:41 +0100

    Parametrize tire pressure tests and improve `tire_status` function with type hints and enhanced docstrings

--- challenges/id_148_tire_pressure/solution.py
+++ challenges/id_148_tire_pressure/solution.py
@@ -1,6 +1,22 @@
 CONVERSION_FACTOR = 14.5038
 
-def tire_status(pressures_psi, range_bar):
+def tire_status(pressures_psi: list, range_bar: list) -> list:
+    """Check the pressure status of the tires.
+
+    The function returns a list of strings indicating the pressure status of each tire.
+    `pressures_psi` is a list of tire pressures in psi. `range_bar` is a list of two
+    floats indicating the minimum and maximum pressure levels for each tire. The
+    function converts the pressures to bar and checks if each pressure is within the
+    specified range.
+
+        Args:
+            pressures_psi (list): A list of tire pressures in psi.
+            range_bar (list): A list of two floats indicating the minimum and maximum
+                pressure levels for each tire.
+
+        Returns:
+            A list of strings indicating the pressure status of each tire.
+        """
     pressures_bar = [pressure / CONVERSION_FACTOR for pressure in pressures_psi]
 
     pressures_eval = []
--- tests/test_148_tire_pressure.py
+++ tests/test_148_tire_pressure.py
@@ -6,17 +6,23 @@ from challenges.id_148_tire_pressure.solution import (
     tire_status,
 )
 
-def test_tire_status_1():
-    assert tire_status([32, 28, 35, 29], [2, 3]) == ["Good", "Low", "Good", "Low"]
-
-def test_tire_status_2():
-    assert tire_status([32, 28, 35, 30], [2, 2.3]) == ["Good", "Low", "High", "Good"]
-
-def test_tire_status_3():
-    assert tire_status([29, 26, 31, 28], [2.1, 2.5]) == ["Low", "Low", "Good", "Low"]
-
-def test_tire_status_4():
-    assert tire_status([31, 31, 30, 29], [1.5, 2]) == ["High", "High", "High", "Good"]
-
-def test_tire_status_5():
-    assert tire_status([30, 28, 30, 29], [1.9, 2.1]) == ["Good", "Good", "Good", "Good"]
+@pytest.mark.parametrize(
+    ("pressures_psi", "range_bar"),
+    [
+        ([32, 28, 35, 29], [2, 3]),
+        ([32, 28, 35, 30], [2, 2.3]),
+        ([29, 26, 31, 28], [2.1, 2.5]),
+        ([31, 31, 30, 29], [1.5, 2]),
+        ([30, 28, 30, 29], [1.9, 2.1]),
+    ],
+    ids=[
+        "tire_status_1",
+        "tire_status_2",
+        "tire_status_3",
+        "tire_status_4",
+        "tire_status_5",
+    ]
+)
+def test_tire_pressure(pressures_psi: list, range_bar: list) -> None:
+    """Test tire status."""
+    assert tire_status(pressures_psi, range_bar)

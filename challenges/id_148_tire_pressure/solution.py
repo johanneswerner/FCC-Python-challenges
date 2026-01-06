2f18bd669b1210f2dc3a13042e6f9db10d2804e8
Johannes Werner <johannes.werner@cogitars.com>
2026-01-06 09:01:47 +0100

    Implement tire pressure status evaluation for challenge 148

--- challenges/id_148_tire_pressure/solution.py
+++ challenges/id_148_tire_pressure/solution.py
@@ -1,3 +1,16 @@
-def tire_status(pressures_psi, range_bar):
+CONVERSION_FACTOR = 14.5038
 
-    return pressures_psi
+def tire_status(pressures_psi, range_bar):
+    pressures_bar = [pressure / CONVERSION_FACTOR for pressure in pressures_psi]
+
+    pressures_eval = []
+    for element in pressures_bar:
+        match element:
+            case _ if element < range_bar[0]:
+                pressures_eval.append("Low")
+            case _ if element > range_bar[1]:
+                pressures_eval.append("High")
+            case _:
+                pressures_eval.append("Good")
+
+    return pressures_eval

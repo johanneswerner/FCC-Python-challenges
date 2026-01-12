5605a85b3245e9a7e3b020baca2099ba481a50f0
Johannes Werner <johannes.werner@cogitars.com>
2026-01-12 20:51:13 +0100

    added first solution for 001

--- challenges/id_001_vowel_balance/solution.py
+++ challenges/id_001_vowel_balance/solution.py
@@ -1,3 +1,15 @@
 def is_balanced(s):
+    vowels = set("aeiou")
+    s = s.lower()
+    length = len(s)
+    first_list = s[:length // 2]
+    second_list = s[(length + 1) // 2:]
 
-    return s
+    sum1, sum2 = 0, 0
+    for first_elem, second_elem in zip(first_list, second_list):
+        if first_elem in vowels:
+            sum1 += 1
+        if second_elem in vowels:
+            sum2 += 1
+
+    return sum1 == sum2
